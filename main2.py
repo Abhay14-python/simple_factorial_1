7a800c93176ea79062b2039c4f91d33674ee0f18
Abhay Kumar Singh <abhay.ksingh.tech2011@gmail.com>
2025-08-04 19:28:19 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,18 @@
+'''This is a programme to print the factorial of the number inputer by the user.'''
+print("This is a programme to print the factorial of the number.")
+while True:
+    try:
+        n = int(input("Enter the number which you have to find the factorial of : "))
+        break
+    except Exception:
+        print("Invalid input \n Please enter a valid input")
+
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial(n-1)
+
+if n < 0:
+    print("Factorial does not exist for negative numbers.")
+else :
+    print(f"The factorial of {n} is {factorial(n)}")
